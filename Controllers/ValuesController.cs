77ede902a565223e4c145aab3e2a87931a438016
fersvcx <rionhuy@gmail.com>
2024-11-07 14:45:19 +0700

    lab1

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SeverGame106.Models;
+
+namespace SeverGame106.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class APIGameController : ControllerBase
+    {
+        public IActionResult Get()
+        {
+            Game106 game106 = new Game106
+            {
+                CourseName = "Back-end Game programing",
+                CourseCode = "GAM106",
+                Name = "Nguyễn Văn A",
+                StudentCode = "PS12345",
+                Class = "GA19305"
+
+            };
+            int status = 1;
+            string message = "get data sucess";
+            var data = new {status, message, game106};
+            return new JsonResult(data);
+        }
+    }
+}
---
+++ Models/Game106.cs
@@ -0,0 +1,13 @@
+﻿namespace SeverGame106.Models
+{
+    public class Game106
+
+    {
+        public string? CourseName { get; set; }
+        public string? CourseCode { get; set; }
+        public string? Name { get; set; }
+        public string? StudentCode { get; set; }
+        public string? Class { get; set; }
+
+    }
+}
